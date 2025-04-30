36e71415228690da4932d573e52d943521ca4da2
hikaru-0124 <2410027@i-seifu.jp>
2025-04-30 10:32:25 +0900

    新規ファイル追加
    ここは詳細な説明を書くとよい

---
+++ Main2.java
@@ -0,0 +1,28 @@
+/*
+C言語で書くと
+#include<stdio.h>
+
+voif main(){
+    printf("Hello World")
+}*/
+
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("こんにちは");
+        System.out.println("じゃぽん");
+        System.out.print("3 + 1 = ");
+        System.out.println(3 + 1);
+        /**PHPで変数を使う場合
+         * $age = 20; 変数$ageに整数値20を代入
+         * echo $age;
+         */
+        int age = 20;
+        double pi = 3.14;
+        System.out.println(age);
+        System.out.println(pi);
+        
+    }
+}
