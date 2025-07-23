8a5a8680f406e16372a40f5a85f26d5afd87315d
hikaru-0124 <2410027@i-seifu.jp>
2025-07-23 13:18:24 +0900

    更新

---
+++ J0723/Main.java
@@ -0,0 +1,13 @@
+package J0723;
+
+import J0723.comment.Zenhan;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Zenhan.doWarusa();
+        Zenhan.doTogame();
+        J0723.comment.Kouhan.callDeae();
+        J0723.comment.Kouhan.showMondokoro();
+    }
+
+   
+}
---
+++ J0723/comment/Kouhan.java
@@ -0,0 +1,13 @@
+package J0723.comment;
+
+public class Kouhan {
+     public static void callDeae(){
+        System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+    }
+    public static void showMondokoro() throws Exception{
+        System.out.println("飛車さん、角さん。もういいでしょう。");
+        System.out.println("この紋所が目に入らぬか");
+        Thread.sleep(1000);
+        Zenhan.doTogame();
+    }
+}
