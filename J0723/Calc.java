96d05258145883d19f2b0cbc729c5016526dfea7
hikaru-0124 <2410027@i-seifu.jp>
2025-07-23 10:28:51 +0900

    あたりあ
    あ

--- J0723/Calc.java
+++ J0723/Calc.java
@@ -1,12 +1,19 @@
 package J0723;
 
 public class Calc {
+<<<<<<< HEAD
     public static void main(String[] args) {
+=======
+     public static void main(String[] args) {
+>>>>>>> e9970b79b31cf1aeb3b2d592a4eec16050f08b85
         int a = 10; int b = 2;
         int total = CalcLogic.tasu(a,b);
         int delta = CalcLogic.hiku(a,b);
         System.out.println("足すと" + total + "引くと" + delta);
     }
 
+<<<<<<< HEAD
   
+=======
+>>>>>>> e9970b79b31cf1aeb3b2d592a4eec16050f08b85
 }
--- J0723/CalcLogic.java
+++ J0723/CalcLogic.java
@@ -1,7 +1,11 @@
 package J0723;
 
 public class CalcLogic {
+<<<<<<< HEAD
       public static int tasu(int a, int b) {
+=======
+    public static int tasu(int a, int b) {
+>>>>>>> e9970b79b31cf1aeb3b2d592a4eec16050f08b85
         return (a + b);
     }
     public static int hiku(int a, int b) {
